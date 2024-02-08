3ff70640c24b3ca34a47f6911b8ee49db27c1bac
FranciscoGontijo <franciscoacmg@gmail.com>
2024-02-08 15:12:12 -0300

    display form with 2 inputs when button is clicked, display div with a text when other button is clicked

--- index.js
+++ index.js
@@ -1,16 +1,54 @@
 
-const changeColorButton = document.querySelector('#changeColorButton');
 const body = document.querySelector('#body-class');
 
-const changeColor = () => {
-    changeColorButton.style.backgroundColor = '#FF1100';
-    body.style.backgroundColor = '#FFF';
+//Open a div with a form
+//Create the form to add bookmarks
+const formContainer = document.createElement('div');
+formContainer.className = 'form-container';
+
+const titleInput = document.createElement('input');
+titleInput.type = 'text';
+titleInput.className = 'text-input';
+
+const urlInput = document.createElement('input');
+urlInput.type = 'text';
+urlInput.className = 'text-input';
+
+formContainer.appendChild(titleInput);
+formContainer.appendChild(urlInput);
+
+const openForm = () => {
+    body.appendChild(formContainer);
 }
+
+//Click add new bookmark button
+//Access the value of both inputs and set it to the new bookmark
+// const bookmarkContainer = document.createElement('div');
+// const bookmarkTitle = document.createElement('h2');
+// bookmarkTitle.innerHTML = 'Bookmark';
+
+const createBookmark = () => {
+    const bookmarkContainer = document.createElement('div');
+    const bookmarkTitle = document.createElement('h2');
+    bookmarkTitle.innerHTML = 'Bookmark';
+
+    bookmarkContainer.appendChild(bookmarkTitle);
+    body.appendChild(bookmarkContainer);
+}
+
+
+//Form has two inputs, one title and one URL
+//use the title to create a h2 inside the new bookmark when submited
+//use the url as a link to the website related with that div
+
 
 const creatNewBookMark = () => {
-   const newDiv = document.createElement('div');
-   newDiv.className = 'bookmark';
-   body.appendChild(newDiv);
+    const newDiv = document.createElement('div');
+    newDiv.className = 'bookmark';
+    body.appendChild(newDiv);
 }
+const createNewBookmarkButton = document.querySelector('#create-bookmark');
 
-changeColorButton.addEventListener('click', creatNewBookMark);
+
+changeColorButton.addEventListener('click', openForm);
+createNewBookmarkButton.addEventListener('click', createBookmark);
